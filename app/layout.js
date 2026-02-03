f9f63cc2e3f700ff8920275fc207297ac0002175
ankitpandor <pandorankit3199@gmail.com>
2026-02-03 21:37:33 +0530

    Navbar logo added in header

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
+import Header from "@/components/Header";
 
 export const metadata = {
   title: "SPOTT",
@@ -12,8 +14,26 @@ export default function RootLayout({ children }) {
         className={`bg-linear-to-br from-gray-950 via-zinc-900 to-stone-900 text-white`}
       >
         {/* {header} */}
-        <main className="relative min-h-screen container mx-auto pt-40 md:pt-32">{children}</main>
+        <Header/>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+        <main className="relative min-h-screen container mx-auto pt-40 md:pt-32">
+          {/* glow */}
+          <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
+            <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl"/>
+            <div className="absolute bottom-0 right-1/5 w-96 h-96 bg-orange-600/20 rounded-full blur-3xl"/>
+          </div>
+          <div className="relative z-10 ">{children}</div>
+        </main>
         {/* {Footer} */}
+        <footer className="border-t border-gray-800/50 py-8 px-6 max-w-7xl mx-auto">
+          <div className="text-sm text-gray-400">Made with 💗 by Ankit Pandor</div>
+        </footer>
+        </ThemeProvider>
       </body>
     </html>
   );
